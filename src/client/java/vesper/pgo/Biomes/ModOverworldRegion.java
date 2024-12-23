eb8c876dacead84c008dd34ccff19e4d45cd3868
Ves <vesmaybevesper@gmail.com>
2024-12-23 12:53:58 -0500

    sync for travel work

--- src/client/java/vesper/pgo/Biomes/ModOverworldRegion.java
+++ src/client/java/vesper/pgo/Biomes/ModOverworldRegion.java
@@ -1,4 +1,28 @@
 package vesper.pgo.Biomes;
 
-public class ModOverworldRegion {
+import com.mojang.datafixers.util.Pair;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.BiomeKeys;
+import net.minecraft.world.biome.source.util.MultiNoiseUtil;
+import terrablender.api.ModifiedVanillaOverworldBuilder;
+import terrablender.api.Region;
+import terrablender.api.RegionType;
+
+import java.util.function.Consumer;
+
+public class ModOverworldRegion extends Region {
+
+    public ModOverworldRegion(Identifier name, int weight) {
+        super(name, RegionType.OVERWORLD, weight);
+    }
+
+    @Override
+    public void addBiomes(Registry<Biome> registry, Consumer<Pair<MultiNoiseUtil.NoiseHypercube, RegistryKey<Biome>>> mapper) {
+        this.addModifiedVanillaOverworldBiomes(mapper, ModifiedVanillaOverworldBuilder -> {
+            ModifiedVanillaOverworldBuilder.replaceBiome(BiomeKeys.LUSH_CAVES, ModBiomes.PALE_CAVE);
+        });
+    }
 }
---
+++ src/client/java/vesper/pgo/datagen/PaleWorldGenerator.java
@@ -0,0 +1,24 @@
+package vesper.pgo.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricDynamicRegistryProvider;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.RegistryWrapper;
+
+import java.util.concurrent.CompletableFuture;
+
+public class PaleWorldGenerator extends FabricDynamicRegistryProvider {
+    public PaleWorldGenerator(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
+        super(output, registriesFuture);
+    }
+
+    @Override
+    protected void configure(RegistryWrapper.WrapperLookup wrapperLookup, Entries entries) {
+        entries.addAll(wrapperLookup.getOrThrow(RegistryKeys.BIOME));
+    }
+
+    @Override
+    public String getName() {
+        return "World Gen";
+    }
+}
